ad3f0c8e59075861e97a59f82b4f06faa7368f82
BergTechWeb <74021308+BergTechWeb@users.noreply.github.com>
2021-03-29 11:49:00 -0300

    Delete trabalho-web-pokemon-depois-da-tamyres-e-dojoaodenovo-main directory

--- trabalho-web-pokemon-depois-da-tamyres-e-dojoaodenovo-main/geolocalizacao.js
+++
@@ -1,9 +0,0 @@
-if('geolocation' in navigator){
-  const watcher = navigator.geolocation.watchPosition(function(position){
-     console.log(position)
-  }, function(error){
-    console.log(error)
-    }, { enableHighAccuracy: true, maximumAge: 30000, timeout: 30000 })
-  }else{
-    alert('localização indisponível')
-  }
